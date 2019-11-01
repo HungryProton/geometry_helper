2b48f78b71606d25334ead000d40b8003948677b
HungryProton <exb2800+github@protonmail.com>
2019-11-01 19:09:29 +0100

    fix: enforce proper rotation / position for the csg mesh

--- tools/polygon_mesh.gd
+++ tools/polygon_mesh.gd
@@ -30,10 +30,11 @@ func _rebuild():
 	csg_mesh = get_node("CSGMesh")
 	if not csg_mesh:
 		csg_mesh = CSGPolygon.new()
-		csg_mesh.transform.basis.x
-		csg_mesh.transform.origin = Vector3.ZERO
 		csg_mesh.set_name("CSGMesh")
 		add_child(csg_mesh)
 		csg_mesh.set_owner(get_tree().get_edited_scene_root())
 
+	# Recenter the csg in case someone moved it
+	csg_mesh.transform.origin = Vector3.ZERO
+	csg_mesh.rotation = Vector3(PI/2.0, 0.0, 0.0)
 	csg_mesh.polygon = polygon_points
